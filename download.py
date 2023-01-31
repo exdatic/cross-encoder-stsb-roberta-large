a5293718742232e07d355684eeb31611e85e528f
Mark Reinke <54356376+loopdeloop76@users.noreply.github.com>
2023-01-31 15:49:11 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,13 +1,11 @@
 # In this file, we define download_model
 # It runs during container build time to get model weights built into the container
-
-# In this example: A Huggingface BERT model
-
-from transformers import pipeline
+from sentence_transformers import CrossEncoder
+import torch
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    model = CrossEncoder('cross-encoder/stsb-roberta-large')
 
 if __name__ == "__main__":
     download_model()
